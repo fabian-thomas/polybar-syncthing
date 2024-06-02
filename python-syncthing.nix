acf305cdc86f3643d6f6cbd92201388543fe0bc1
Fabian Thomas <fabian@fabianthomas.de>
2024-06-02 19:10:57 +0200

    fix python-syncthing with nixos-24.05

--- python-syncthing.nix
+++ python-syncthing.nix
@@ -3,6 +3,7 @@
 , setuptools
 , requests
 , dateutil
+, applyPatches
 }:
 
 buildPythonPackage rec {
@@ -10,12 +11,15 @@ buildPythonPackage rec {
   version = "2.4.3";
   format = "pyproject";
 
-  src = fetchFromGitHub {
-    owner = "blakev";
-    repo = "python-syncthing";
-    rev = "0d1e968a9d3a15f152a67b053b98a50c7361cd20";
-    hash = "sha256-QEwkwnUq8RH5ntQSnlEjzjijRGktZaiK/BSEQc0kM+s=";
-  };
+  src = (applyPatches {
+    src = (fetchFromGitHub {
+        owner = "blakev";
+        repo = "python-syncthing";
+        rev = "0d1e968a9d3a15f152a67b053b98a50c7361cd20";
+        hash = "sha256-QEwkwnUq8RH5ntQSnlEjzjijRGktZaiK/BSEQc0kM+s=";
+    });
+    patches = [ ./python-syncthing.patch ];
+  });
 
   nativeBuildInputs = [
     setuptools
